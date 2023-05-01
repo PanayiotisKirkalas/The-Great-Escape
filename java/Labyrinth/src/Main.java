2576a34319aacd4a0a3eb1eaacd0e5b2525bdebc
DionisisHoxha <128989383+DionisisHoxha@users.noreply.github.com>
2023-05-01 21:04:00 +0300

    Kirkalas: Added main class

--- java/Labyrinth/src/Main.java
+++ java/Labyrinth/src/Main.java
@@ -1,177 +1,165 @@
+import java.awt.event.KeyEvent;
 import java.io.*;
 import java.util.*;
 
-public class Main
-{
-	public static Scanner keyboard = new Scanner(System.in).useDelimiter("\\n");
-	
-	public static int MIN_PLAYERS = 2;
-	public static int MAX_PLAYERS = 4;
-	
-	static boolean MainMenu2(boolean go_a, boolean Mode)
-	{
-		int ch, temp;
-		int index = 0;
-		int n = 0;
-		
-		while (true)
-		{
-			System.out.print("\033[2J\033[1;1H");
+
+public class Main {
+
+	boolean MainMenu2(boolean go_a, boolean Mode) {
+		char ch;
+		int index = 0, n = 0;
+		while (true) {
+			System.out.print("\033[H\033[2J");//clear screen;;
 			System.out.println("MAIN MENU");
 			switch (index)
 			{
-				case 0:
-					System.out.println("    ->Play");
-					if (go_a) System.out.println(" again");
-					System.out.println("      How to play");
-					System.out.println("      Back");
-					break;
-					
-				case 1:
-					System.out.println("      Play");
-					if (go_a) System.out.println(" again");
-					System.out.println("    ->How to play");
-					System.out.println("      Back");
-					break;
-					
-				case 2:
-					System.out.println("      Play");
-					if (go_a) System.out.println(" again");
-					System.out.println("    ->How to play");
-					System.out.println("      Back");
-					break;
-					
-				default:
-					break;
-			}
-			
-			ch = (int)keyboard.next().charAt(0);
-			if (ch == 13)
-			{
-				switch (index)
-				{
-					case 0:
-						go_a = false;
-						
-						if (!Mode)
-						{
-							Mode1 m1 = new Mode1();
-							m1.Setup();
-							m1.Play();
-							go_a = true;
-							break;
-						}
-						
-						do
-						{
-							System.out.print("\033[2J\033[1;1H");
-							System.out.println("Number of players (" + MIN_PLAYERS + "-" + MAX_PLAYERS + "): " + n);
-							n = keyboard.nextInt();
-						} while (n > MAX_PLAYERS || n < MIN_PLAYERS);
-						{
-							Mode2 m2 = new Mode2();
-							m2.Setup();
-							m2.Play();
-						}
-						go_a = true;
-						break;
-						
-					case 1:
-						System.out.print("\033[2J\033[1;1H");
-						if (!Mode)
-						{
-							Mode1 m1 = new Mode1();
-							System.out.println(m1.Explanation);
-						}
-						else
-						{
-							Mode1 m2 = new Mode2();
-							System.out.println(m2.Explanation);
-						}
-						while((temp = (int)keyboard.next().charAt(0)) != 13)
-							temp = (int)keyboard.next().charAt(0);
-						System.out.print("\033[2J\033[1;1H");
-						break;
-						
-					case 2:
-						return true;
-						
-					default:
-						break;
+			case 0:
+				System.out.print("    ->Play");
+				if (go_a) {
+					System.out.print(" again"); //+ 1 tab
 				}
-			}
-			else if (ch == 119) // ASCII w
-			{
-				if (index == 0)
-					index = 2;
-				else
-					index--;
-			}
-			else if (ch == 115) // ASCII s
-			{
-				if (index == 2)
-					index = 0;
-				else
-					index++;
-			}
-		}
-	}
-	
-	static boolean MainMenu(boolean go_a)
-	{
-		int ch;
-		int index = 0;
-		while (true)
-		{
-			System.out.print("\033[2J\033[1;1H"); // equivelant to system("cls");
-			System.out.println("MAIN MENU");
-			switch (index)
-			{
-				case 0:
-					System.out.println("    ->1v1");
-					System.out.println("      Everyone for themselves (up to 4 players)");
-					System.out.println("	  Quit");
-					break;
-				case 1:
-					System.out.println("      1v1");
-					System.out.println("    ->Everyone for themselves (up to 4 players)");
-					System.out.println("	  Quit");
-					break;
-				case 2:
-					System.out.println("      1v1");
-					System.out.println("      Everyone for themselves (up to 4 players)");
-					System.out.println("	->Quit");
-					break;
+				System.out.print('\n' + "      How to play" + '\n' + "      Back");
+				break;
+				
+			case 1:
+				System.out.print("      Play");
+				if (go_a) {
+					System.out.print(" again"); //+ 1 tab
+				}
+				System.out.print('\n' + "    ->How to play" + '\n' + "      Back");
+				break;
+
+			case 2:
+				System.out.print("      Play");
+				if (go_a) {
+					System.out.print(" again"); //+ 1 tab
+				}
+				System.out.print('\n' + "      How to play" + '\n' + "    ->Back");
+				break;
+
+			default:
+				break;
 			}
 
-			ch = (int)keyboard.next().charAt(0);
-			if (ch == 13)
-			{
-				switch (index)
-				{
-					case 0:
-						go_a = false;
-						MainMenu2(go_a, false);
-						break;
-					case 1:
-						go_a = false;
-						MainMenu2(go_a, true);
-						break;
-					case 2:
-						return true;
-					default:
-						break;
+			while (true) {
+				if ((ch = KeyEvent.KEY_PRESSED) == KeyEvent.VK_ENTER) {
+					break;
 				}
 			}
-			else if (ch == 119) // ASCII w
-			{
+			if (ch == KeyEvent.VK_ENTER) {
+				switch (index)
+				{
+				case 0:
+					go_a = false;
+
+					if (!Mode) {
+						Mode1 m1 = new Mode1();
+						m1.Setup();
+						m1.Play();
+						go_a = true;
+						break;
+					}
+					
+					do {
+						System.out.print("\033[H\033[2J");//clear screen;;
+						System.out.print("Number of players (" + 2 + '-' + 4 + "): ");
+						n = new Scanner(System.in).nextInt();
+					} while (n > 4 || n < 2);
+					{
+						Mode2 m2 = new Mode2(n);
+						m2.Setup();
+						m2.Play();
+					}
+					go_a = true;
+					break;
+
+				case 1:
+					System.out.print("\033[H\033[2J");//clear screen;;
+					if (!Mode) {
+						System.out.print(Mode1.Explanation);
+					}
+					else{
+						System.out.print(Mode2.Explanation);
+					}
+					new Scanner(System.in).nextLine();//pause
+					System.out.print("\033[H\033[2J");//clear screen;;
+					break;
+
+				case 2:
+					return true;
+
+				default:
+					break;
+				}
+			}
+			else if (ch == 'w'/* && index > 0*/) {
 				if (index == 0)
 					index = 2;
 				else
 					index--;
 			}
-			else if (ch == 115) // ASCII s
-			{
+			else if (ch == 's'/* && index < 2*/) {
+				if (index == 2)
+					index = 0;
+				else
+					index++;
+			}
+		}
+	}
+
+	boolean MainMenu(boolean go_a) {
+		char ch;
+		int index = 0;
+		while (true) {
+			System.out.print("\033[H\033[2J");//clear screen;;
+			System.out.println("MAIN MENU");
+			switch (index) {
+			case 0:
+				System.out.println("    ->1v1"); //+ 1 tab
+				System.out.println("      Everyone for themselves (up to 4 players)" + '\n' + "      Quit");
+				break;
+			case 1:
+				System.out.println("      1v1"); //+ 1 tab
+				System.out.println("    ->Everyone for themselves (up to 4 players)" + '\n' + "      Quit");
+				break;
+			case 2:
+				System.out.println("      1v1"); //+ 1 tab
+				System.out.println("      Everyone for themselves (up to 4 players)" + '\n' + "    ->Quit");
+				break;
+			}
+
+			while (true) {
+				if ((ch = KeyEvent.KEY_PRESSED) == KeyEvent.VK_ENTER) {
+					break;
+				}
+			}
+			if (ch == KeyEvent.VK_ENTER) {
+				switch (index)
+				{
+				case 0:
+					go_a = false;
+					MainMenu2(go_a, false);
+					break;
+
+				case 1:
+					go_a = false;
+					MainMenu2(go_a, true);
+					break;
+
+				case 2:
+					return true;
+
+				default:
+					break;
+				}
+			}
+			else if (ch == 'w'/* && index > 0*/) {
+				if (index == 0)
+					index = 2;
+				else
+					index--;
+			}
+			else if (ch == 's'/* && index < 2*/) {
 				if (index == 2)
 					index = 0;
 				else
@@ -180,9 +168,9 @@ public class Main
 		}
 	}
 	
-	public static void main(String[] args)
-	{
-		boolean ggo = false;
-		MainMenu(ggo);
+	public static void main(String[] args) {
+		
+
 	}
+
 }
